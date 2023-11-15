632061deb4d0ebcdc1a0e5581f919776a27f8743
Stanislau Lihodievski <s.lihodievski@richbrains.net>
2023-11-15 21:00:16 +0200

    fix bug with without saving theme

--- src/app/providers/ThemeProvider/ui/ThemsProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemsProvider.tsx
@@ -2,7 +2,7 @@ import {type FC, type ReactNode, useState, useMemo} from 'react';
 import {LOCAL_STORAGE_THEME_DATA, Theme, ThemsContext} from '../lib/ThemsContext';
 
 const storedState = JSON.parse(localStorage.NAVIGATION_UI_STATE || '{}');
-const defaultTheme = storedState.LOCAL_STORAGE_THEME_DATA as Theme || Theme.LIGHT;
+const defaultTheme = storedState[LOCAL_STORAGE_THEME_DATA] as Theme || Theme.LIGHT;
 type Props = {
 	children: ReactNode;
 };
