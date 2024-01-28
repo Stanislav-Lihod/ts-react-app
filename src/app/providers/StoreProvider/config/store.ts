63999f662836cbd8e9b2a708f7337843f9adb1d1
Stanislau Lihodievski <s.lihodievski@richbrains.net>
2024-01-29 00:09:07 +0200

    Edit profile page and cover it async reducer

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,14 +2,12 @@ import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchems';
-import {profileReducer} from "entities/Profile";
 import {createReducerManager} from "app/providers/StoreProvider/config/ReducerManager";
 
 export function createReduxStore(initialState?: StateSchema) {
   const rootReducers: ReducersMapObject<StateSchema> = {
     counter: counterReducer,
     user: userReducer,
-    profile: profileReducer
   };
 
   const reducerManager = createReducerManager(rootReducers)
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,12 +1,22 @@
+import {classNames} from "shared/lib/className/classNames";
+import {DynamicModuleLoader, ReducersList} from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
+import {profileReducer} from "entities/Profile";
+
 interface ProfilePageProps{
   className?: string
 }
 
+const initialReducer : ReducersList = {
+  profile: profileReducer
+}
+
 const ProfilePage = ({className}: ProfilePageProps) => {
   return (
-    <div>
-      Profile page
-    </div>
+    <DynamicModuleLoader reducers={initialReducer} removeReducerAfterUnmount={true}>
+      <div className={classNames('', {}, [className])}>
+        Profile page
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
